eea9a2e938ad20d07b3d8681e1445a39cf11fac5
patalena <lenapat0212@gmail.com>
2026-04-09 21:11:45 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,43 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size] = {};
+    int top = -1;
+public:
+    TStack() {}
+    bool empty() const {
+        return top == -1;
+    }
+    bool full() const {
+        return top == size - 1;
+    }
+    void push(const T& item) {
+        if (!full()) {
+            arr[++top] = item;
+        }
+    }
+    void pop() {
+        if (!empty()) {
+            top--;
+        }
+    }
+    T get() const {
+        if (!empty()) {
+            return arr[top];
+        }
+        return T();
+    }
+    T popget() {
+        if (!empty()) {
+            T val = arr[top];
+            top--;
+            return val;
+        }
+        return T();
+    }
+    void clear() {
+        top = -1;
+    }
 };
-
-#endif  // INCLUDE_TSTACK_H_
+#endif
